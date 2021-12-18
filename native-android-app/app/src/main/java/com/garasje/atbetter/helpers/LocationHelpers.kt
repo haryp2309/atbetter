cd628f57799f313cd5b6bf6e7af8e8a6eee66743
haryp2309 <hary2309@gmail.com>
2021-12-18 21:09:29 +0100

    Implemented GMS-independent location access. Fixes #7.

--- native-android-app/app/src/main/java/com/garasje/atbetter/helpers/LocationHelpers.kt
+++ native-android-app/app/src/main/java/com/garasje/atbetter/helpers/LocationHelpers.kt
@@ -1,25 +1,32 @@
 package com.garasje.atbetter.helpers
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.content.Context.LOCATION_SERVICE
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.LocationServices
 
 object LocationHelpers {
-    fun onLocationAccess(activity: Activity, onSuccess: (location: Location) -> Unit) {
-        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
 
-        if (ActivityCompat.checkSelfPermission(
-                activity,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                activity,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+    const val LOCATION_REFRESH_TIME = 10000L // in ms
+    const val LOCATION_REFRESH_DISTANCE = 100F // in meter
+
+    fun onLocationAccess(activity: AppCompatActivity, onSuccess: (location: Location) -> Unit) {
+
+        val coarseLocationPermission = ActivityCompat.checkSelfPermission(
+            activity,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+
+        if (coarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
             val reqCode = 123 // I don't know what this is (haryp2309)
             ActivityCompat.requestPermissions(
                 activity,
@@ -29,13 +36,24 @@ object LocationHelpers {
                 ), reqCode
             )
         }
-        fusedLocationProviderClient.lastLocation
+
+
+        LocationServices.getFusedLocationProviderClient(activity)
+            .lastLocation
             .addOnSuccessListener { location: Location? ->
                 // Got last known location. In some rare situations this can be null.
                 if (location != null) {
                     onSuccess(location)
                 }
             }
+
+        val locationManager = activity.getSystemService(LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            LOCATION_REFRESH_TIME,
+            LOCATION_REFRESH_DISTANCE,
+            onSuccess
+        )
 
     }
 }
